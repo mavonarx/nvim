c688fc6b7dc31ec3e8b30fd8fd760a38e7d390da
maettu102 <matthias.von-arx@gmx.ch>
2023-12-06 00:28:36 +0100

    install bash script

---
+++ install.sh
@@ -0,0 +1,3 @@
+rm -rf ~/.cache/nvim ~/.local/share/nvim/ ~/.config/nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+ln -s ~/.config/nvim_myconf/custom ~/.config/nvim/lua/custom
